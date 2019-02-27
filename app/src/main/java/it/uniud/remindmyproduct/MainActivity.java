e80adc95f957e14e6b52ce58a88d7cd3b0a39a9c
samdigital <sam@samdigital.eu>
2019-02-27 22:28:20 +0100

    sistemato il main aggiunte icone un po' di tutto

--- app/src/main/java/it/uniud/remindmyproduct/MainActivity.java
+++ app/src/main/java/it/uniud/remindmyproduct/MainActivity.java
@@ -1,16 +1,61 @@
 package it.uniud.remindmyproduct;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawer;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_drawer);
 
 
+        toolbar=(Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("Remind My Product");
 
+        drawer=(DrawerLayout) findViewById(R.id.main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView=(NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.drawer_home:
+                        //Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
+                        drawer.closeDrawers();
+                        //startActivity(intent);
+                        return true;
+
+                    case R.id.drawer_report:
+                        Intent intent = new Intent(getApplicationContext(), ReportActivity.class);
+                        drawer.closeDrawers();
+                        startActivity(intent);
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 }
